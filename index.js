d82cac8e87b6a83b4c0c81abb8980b010c333a89
Maros Kollar <maros@k-1.com>
2015-10-31 23:31:54 +0100

    First draft

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ function ThermostatControl (id, controller) {
     // Call superconstructor first (AutomationModule)
     ThermostatControl.super_.call(this, id, controller);
     
-    this.langFile       = undefined;
+    this.langFile           = undefined;
+    this.minTemperature     = undefined;
+    this.maxTemperature     = undefined;
+    this.timeouts           = [];
 }
 
 inherits(ThermostatControl, AutomationModule);
@@ -28,18 +31,17 @@ ThermostatControl.prototype.init = function (config) {
     ThermostatControl.super_.prototype.init.call(this, config);
     var self = this;
     
-    self.langFile = self.controller.loadModuleLang("ThermostatControl");
+    self.langFile           = self.controller.loadModuleLang("ThermostatControl");
     
     // Create vdev
     this.vDev = this.controller.devices.create({
         deviceId: "ThermostatControl_" + this.id,
         defaults: {
-            deviceType: 'thermostat',
             metrics: {
                 scaleTitle: config.unitTemperature === "celsius" ? '°C' : '°F',
-                level: 19,
-                min: config.minTemperature,
-                max: config.maxTemperature,
+                level: config.defaultTemperature,
+                min: parseFloat(config.globalLimit.minTemperature),
+                max: parseFloat(config.globalLimit.maxTemperature),
                 icon: '',
                 title: langFile.title
             },
@@ -52,6 +54,13 @@ ThermostatControl.prototype.init = function (config) {
         },
         moduleId: this.id
     });
+    
+    self.callbackEvent = _.bind(self.calculateSetpoint,self);
+    _.each(self.presenceStates,function(presenceState) {
+        self.controller.on("presence."+presenceState, self.callbackEvent);
+    });
+    
+    setTimeout(self.callbackEvent,60000,'init');
 };
 
 ThermostatControl.prototype.stop = function() {
@@ -63,40 +72,223 @@ ThermostatControl.prototype.stop = function() {
         self.controller.devices.remove(self.vDev.id);
         self.vDev = undefined;
     }
+    
+    _.each(self.presenceStates,function(presenceState) {
+        self.controller.off("presence."+presenceState, self.callbackEvent);
+    });
+    self.callbackEvent = undefined;
 };
 
 // ----------------------------------------------------------------------------
 // --- Module methods
 // ----------------------------------------------------------------------------
 
-ThermostatControl.prototype.setLevel = function(level) {
+ThermostatControl.prototype.presenceStates = ['home','night','away','vacation'];
+
+ThermostatControl.prototype.calculateSetpoint = function(source) {
     var self = this;
     
-    if (level > self.config.maxTemperature) {
-        level = self.config.maxTemperature;
-    } else if (level < self.config.minTemperature) {
-        level = self.config.minTemperature;
-    }
+    console.error('[ThermostatControl] Calculating setpoints due to '+source);
     
-    this.set("metrics:level", level);
-    
-    _.each(self.devices,function(element) {
-        var deviceLevel = level;
-        var deviceId = element.device;
-        var offset = element.offset || 0;
-        var maxTemperature = element.maxTemperature || self.config.maxTemperature;
-        var minTemperature = element.minTemperature || self.config.minTemperature;
-        var device = self.controller.devices.get(deviceId);
-        
-        deviceLevel = deviceLevel + offset;
-        
-        if (deviceLevel > maxTemperature) {
-            deviceLevel = maxTemperature;
-        } else if (deviceLevel < minTemperature) {
-            deviceLevel = minTemperature;
+    var dateNow         = new Date();
+    var dayNow          = dateNow.getDay();
+    var presenceNow     = self.presenceMode();
+    var setpoint        = self.vDev.get('metrics:level');
+    var globalSetpoint  = self.config.defaultTemperature;
+    var evalSchedule    = function(schedule) {
+        // Check presence mode
+        var presenceMode = schedule.presenceMode;
+        if (typeof(presenceMode) === 'array' 
+            && presenceMode.length > 0
+            && ! _.find(presenceMode, presenceNow)) {
+            return false;
         }
         
-        device.set('metrics:level',deviceLevel)
+        // Check day of week if set
+        var dayofweek = schedule.dayofweek;
+        if (typeof(dayofweek) === 'array' 
+            && dayofweek.length > 0
+            && ! _.find(dayofweek, dayNow.toString())) {
+            return false;
+        }
+        
+        // Check from/to time
+        var timeFrom    = self.parseTime(schedule.timeFrom);
+        var timeTo      = self.parseTime(schedule.timeTo);
+        if (typeof(timeFrom) === 'undefined'
+            || typeof(timeTo) === 'undefined') {
+            return false;
+        }
+        if (timeFrom > dateNow || dateNow > timeTo) {
+            return false;
+        }
+        return true;
+    };
+    
+    
+    
+    _.find(self.config.globalSchedules,function(schedule) {
+        if (evalSchedule(schedule) == false) {
+            return;
+        }
+        
+        if (schedule.mode === 'absolute') {
+            globalSetpoint = parseFloat(schedule.setpoint);
+        } else if (schedule.mode === 'relative') {
+            globalSetpoint = self.config.defaultTemperature + parseFloat(schedule.setpoint)
+        }
+        return globalSetpoint;
     });
+    
+    if (source !== 'setpoint' 
+        && ! source.match(/^zone\.[0-9]+$/)) {
+        console.error('[ThermostatControl] Setting global to '+globalSetpoint);
+        self.vDev.set('metrics:level',globalSetpoint);
+    }
+    
+    _.each(self.config.zones,function(zone,index) {
+        var zoneSetpoint = globalSetpoint;
+        _.find(zone.schedule,function(schedule) {
+            if (evalSchedule(schedule) == false) {
+                return;
+            }
+            if (schedule.mode === 'absolute') {
+                zoneSetpoint = parseFloat(schedule.setpoint);
+            } else if (schedule.mode === 'relative') {
+                zoneSetpoint = globalSetpoint + parseFloat(schedule.setpoint)
+            }
+            return zoneSetpoint;
+        });
+        level = self.checkLimit(level,zone.limit);
+        
+        _.each(self.config.devices,function(device) {
+            var deviceObject = self.controller.devices.get(device);
+            console.error('[ThermostatControl] Setting '+deviceObject.get('metrics:title')+' to '+zoneSetpoint);
+            deviceObject.performCommand('exact',{ 'level': zoneSetpoint });
+        });
+    });
+    
+    self.initTimeouts();
+};
+
+ThermostatControl.prototype.presenceMode = function() {
+    var self = this;
+    
+    var presenceMode;
+    self.controller.devices.each(function(vDev) {
+        if (vDev.get('deviceType') === 'deviceType'
+            && vDev.get('metrics:probeTitle') === 'presence') {
+            presenceMode = vDev.get('metrics:mode');
+        }
+    });
+    
+    if (typeof(presenceMode) === 'undefined') {
+        console.error('[ThermostatControl] Could not find presence device');
+        return;
+    }
+    
+    return presenceMode;
+};
+
+ThermostatControl.prototype.initTimeouts = function() {
+    var self = this;
+    
+    _.each(self.timeouts,function(timeout) {
+        clearTimeout(timeout);
+    });
+    self.timeouts = [];
+    
+    var dateNow     = new Date();
+    
+    _.each(self.config.globalSchedules,function(schedule) {
+        var timeout = self.calculateTimeout(schedule);
+        if (typeof(timeout) !== 'undefined') {
+            self.timeouts.push(setTimeout(self.callbackEvent,timeout,'global'));
+        }
+    });
+    
+    _.each(self.config.zones,function(zone,index) {
+        _.each(zone.schedules,function(schedule) {
+            var timeout = self.calculateTimeout(schedule);
+            if (typeof(timeout) !== 'undefined') {
+                self.timeouts.push(setTimeout(self.callbackEvent,timeout,'zone.'+index));
+            }
+        });
+    });
+};
+
+ThermostatControl.prototype.parseTime = function(timeString) {
+    var match       = timeString.match(/^(\d{1,2}):(\d{1,2})$/);
+    if (!match) {
+        return;
+    }
+    var hour        = parseInt(match[1]);
+    var minute      = parseInt(match[2]);
+    var dateCalc    = new Date();
+    dateCalc.setHours(hour, minute);
+    
+    return dateCalc;
+};
+
+
+ThermostatControl.prototype.calculateTimeout = function(setpoint) {
+    var self = this;
+    
+    var dateNow     = new Date();
+    var dayofweek   = setpoint.dayofweek;
+    var timeFrom    = setpoint.timeFrom;
+    var timeTo      = setpoint.timeTo;
+    var results     = [];
+    
+    if (typeof(timeFrom) === 'undefined'
+        && typeof(timeTo) === 'undefined') {
+        return;
+    }
+    
+    _.each([timeFrom,timeTo],function(timeString) {
+        var dateCalc = self.parseTime(timeString);
+        while (dateCalc < dateNow 
+            || (
+                typeof(dayofweek) === 'array'
+                && dayofweek.length > 0 
+                && ! _.find(dayofweek, dateCalc.getDay().toString())
+            )) {
+            dateCalc.setHours(dateCalc.getHours() + 24);
+            dateCalc.setHours(hour);
+        }
+        results.push();
+    });
+    
+    if (results.length === 0) {
+        return;
+    }
+        
+    results.sort();
+    console.log('[ThermostatControl] '+results[0]);
+    return (results[0].getTime() - dateNow.getTime());
+};
+
+ThermostatControl.prototype.checkLimit = function(level,limit) {
+    var self = this;
+    level = parseFloat(level);
+    
+    if (typeof(limit) !== 'object') {
+        return level;
+    }
+    if (typeof(limit.maxTemperature) !== 'undefined'
+        && level > limit.maxTemperature) {
+        level = limit.maxTemperature;
+    } else if (typeof(limit.minTemperature) !== 'undefined'
+        && level < limit.minTemperature) {
+        level = limit.minTemperature;
+    }
+    return level;
+};
+
+ThermostatControl.prototype.setLevel = function(level) {
+    var self = this;
+    level = self.checkLimit(level,self.config.globalLimit);
+    this.set("metrics:level", level);
+    self.calculateSetpoint('setpoint');
 };
  
